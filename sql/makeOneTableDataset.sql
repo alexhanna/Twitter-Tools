02265a13ee66af00c7e0b7d69138c2ac6ab1fd75
Alex Hanna <alex.hanna@gmail.com>
2011-10-14 10:28:01 -0500

    commit

---
+++ sql/makeOneTableDataset.sql
@@ -0,0 +1,24 @@
+SELECT
+    t.status_id  AS "Tweet ID",
+    t.text       AS "Text",
+    t.geo        AS "Geolocation",
+    t.source     AS "Source",
+    t.retweet_id AS "Retweeted Tweet ID",
+    t.created_at AS "Created at",
+    
+    t.user_id     AS "User ID",
+    u.screen_name AS "Screen Name",
+    u.name        AS "Name",
+    u.description AS "User Description",
+    u.url         AS "User URL",
+    u.lang        AS "User Language",
+    u.time_zone   AS "User Timezone",
+    s.created_at  AS "User Created At"
+
+    u.followers_count AS "User Follow Count",
+    u.friends_count   AS "User Followee Count",
+    u.statuses_count  AS "User Status Count",
+    
+      FROM tweet t
+INNER JOIN tweet_userinfo  u INNER JOIN (t.status_id = u.status_id)
+INNER JOIN static_userinfo s INNER JOIN (t.user_id   = s.user_id)
